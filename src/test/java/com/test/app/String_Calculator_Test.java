4d55c640a8c2358ddf214baf29f837207de9e24a
Shubham <shubhamv2010@gmail.com>
2021-10-17 21:38:43 +0530

    Test: Number Greater than 1000

--- src/test/java/com/test/app/String_Calculator_Test.java
+++ src/test/java/com/test/app/String_Calculator_Test.java
@@ -11,54 +11,69 @@ class String_Calculator_Test {
 	StringCalculator cal=new StringCalculator();
 	
 	@Test
-	void test_null_number() {
+	void test_Null_Number() {
 		assertEquals(0,cal.add(""));
 	}
 	
 	@Test
-	void test_singleNumber() {
+	void test_SingleNumber() {
 		assertEquals(1,cal.add("1"));
 	}
 	
 	@Test
-	void test_singleNumber_2() {
+	void test_SingleNumber_2() {
 		assertEquals(2,cal.add("2"));
 	}
 	
 	@Test
-	void test_twonumber() {
+	void test_TwoNumber() {
 		assertEquals(5,cal.add("2,3"));
 	}
 	
 	@Test
-	void test_3_number() {
+	void test_3_Number() {
 		assertEquals(12,cal.add("2,4,6"));
 	}
 	
 	@Test
-	void test_3_number_withnewline_andcomma() {
+	void test_3_Number_WithNewLine_andcomma() {
 		assertEquals(12,cal.add("2\n4,6"));
 	}
 	
 	@Test
-	void test_3_number_withnewline() {
+	void test_3_Number_WithNewLine() {
 		assertEquals(12,cal.add("2\n4\n6"));
 	}
 	
 	
 	@Test
-	void negativenumber_test1() {
+	void negativeNumber_test1() {
 		cal.add("-2,1");
 	}
 	
 	@Test
-	void negativenumber_test2() {
+	void negativeNumber_test2() {
 		cal.add("-2,-1");
 	}
 	
 	@Test
-	void negativenumber_test3() {
+	void negativeNumber_test3() {
 		cal.add("-2,-1,-11");
+	}
+	
+	@Test
+	void numberGreaterThan1k_Test1() {
+		assertEquals(2,cal.add("2,1002"));
+	}
+	
+	@Test
+	void numberGreaterThan1k_Test2() {
+		assertEquals(0,cal.add("1004,1002"));
+	}
+	
+	@Test
+	void numberGreaterThan1k_Test3() {
+		assertEquals(2,cal.add("1004,1002,2"));
 	}
 	
 	
